e305b20d0470369f0f86c49ef28dd06aa4a95601
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-12-08 11:55:23 -0700

    add api consumption

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem "rails", "~> 7.0.8"
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
 
+gem "faraday"
+
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
 class WelcomeController < ApplicationController
   def index
-    render json: {"success": "hello :)"}
+    conn = Faraday.new(
+      url: "https://api.unsplash.com",
+      params: {client_id: Rails.application.credentials.unsplash[:client_id]}
+    )
+
+    response = conn.get("/photos/random/?query=old person")
+    parsed = JSON.parse(response.body)
+    render json: parsed
   end
 end
