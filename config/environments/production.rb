bc91e9468d4c08438992c1585e818902d750bf3c
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-12-08 11:31:27 -0700

    add new host name for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "docker-test-be2307-1675c153078a.herokuapp.com"
+  config.hosts << "docker-test-be2307-prod-356433bfa246.herokuapp.com"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
